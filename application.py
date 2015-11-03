83d8fdf4dd0104c8a47c5602d0a08bbdcd94bbbf
kokhouser <hzk11a@acu.edu>
2015-11-03 15:26:37 -0600

    Redid directories

--- application.py
+++ application.py
@@ -1,26 +1,11 @@
-#!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask,render_template, flash, redirect, session, url_for,request
+from server import application, db, models
+from sqlalchemy.sql import exists
 
 application = Flask(__name__)
+application.debug=True
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
-@application.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+application.secret_key = 'geocash' 
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    application.run()
--- config.py
+++ config.py
@@ -1,11 +1,8 @@
-import os                                                                                             
-# SQLAlchemy database configuration. Here we are using a local sqlite3
-# database
+import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 WTF_CSRF_ENABLED = True
+SECRET_KEY = 'geocash'
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')                                                                      
-# Generate a random secret key
-SECRET_KEY = os.urandom(24)
-# Disable debugging
-DEBUG = True  
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
