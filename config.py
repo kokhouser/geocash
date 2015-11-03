3ee6388d652fbc0690d6bd2e25956bdf01e845c4
kokhouser <hzk11a@acu.edu>
2015-11-03 15:02:28 -0600

    Changed directory structure

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
-import os 
+import os                                                                                             
+# SQLAlchemy database configuration. Here we are using a local sqlite3
+# database
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'geocash'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')                                                                      
+# Generate a random secret key
+SECRET_KEY = os.urandom(24)
+# Disable debugging
+DEBUG = True  
